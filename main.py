848508db9fdc4eff2a97c2b9a4760cabc39e8168
BrandonTang <blt264@nyu.edu>
2017-02-18 12:49:54 -0500

    Setup firebase connection, setup maps

--- main.py
+++ main.py
@@ -1,97 +1,52 @@
 import os
+import requests
+import json
 import hashlib
+import pyrebase
 import sqlite3 as sql
 from flask import Flask, render_template, request, session, url_for, redirect, flash
+from flask_googlemaps import GoogleMaps, Map
 
 # Initialize the app from Flask
 app = Flask(__name__)
 app.secret_key = 'secret_key'
 
 
+config = {
+  "apiKey": "AIzaSyAouz-Hjy7z8fu-8zIlYs4Ay4tfLURAUYw",
+  "authDomain": "chalkitup-5b411.firebaseapp.com",
+  "databaseURL": "https://chalkitup-5b411.firebaseio.com",
+  "storageBucket": "chalkitup-5b411.appspot.com",
+  "messagingSenderId": "933786712703"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+app.config['GOOGLEMAPS_KEY'] = "AIzaSyA7PFRZw7yC2TcwR0Rm8k8V0l-MSZt4aRk"
+GoogleMaps(app)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     """
-    Return the home page that displays all uploaded images and their captions.
+    Return the home page for commUnity that displays about, download, team, and maps sections.
     """
-    logged_in = False
-    if session.get('logged_in') is True:
-        logged_in = True
-    username = session.get('username')
-    return render_template('index.html', logged_in=logged_in, username=username)
-
-
-@app.route('/login')
-def login():
-    """
-    Return the login page that calls login_auth on submit.
-    """
-    return render_template('login.html')
-
-
-@app.route('/login_auth', methods=['GET', 'POST'])
-def login_auth():
-    """
-    Authenticates user credentials, creates session for user if valid, else redirects to login with flash message.
-    """
-    username = request.form['username']
-    password = request.form['password']
-    md5password = hashlib.md5(password.encode('utf-8')).hexdigest()
-    conn = sql.connect("appsec.db", timeout=10)
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, md5password))
-    user = cursor.fetchone()
-    conn.commit()
-    conn.close()
-    if user:
-        session['username'] = username
-        session['logged_in'] = True
-        flash('User successfully logged in!', category='success')
-        return redirect(url_for('index'))
-    else:
-        flash('Invalid login or username or password.', category='error')
-        return redirect(url_for('login'))
-
-
-@app.route('/register')
-def register():
-    """
-    Return the register page that calls register_auth on submit.
-    """
-    return render_template('register.html')
-
-
-@app.route('/register_auth', methods=['GET', 'POST'])
-def register_auth():
-    """
-    Checks if user already exists, redirects to register page if true, else creates new user.
-    """
-    username = request.form['username']
-    password = request.form['password']
-    md5password = hashlib.md5(password.encode('utf-8')).hexdigest()
-    conn = sql.connect("appsec.db", timeout=10)
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
-    user = cursor.fetchone()
-    conn.commit()
-    conn.close()
-    if user:
-        flash('User already exists.', category='error')
-        return redirect(url_for('register'))
-    else:
-        insert_user(username, md5password)
-        flash('User successfully registered! You may login now.', category='success')
-        return redirect(url_for('login'))
-
-
-@app.route('/logout')
-def logout():
-    """
-    Logs out the user and removes information from session, then redirects to index page.
-    """
-    session.pop('username')
-    session.pop('logged_in')
-    flash('User successfully logged out.', category='success')
-    return redirect('/')
+    # Maps Section
+    mymap = Map(
+        identifier="view-side",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[(37.4419, -122.1419)],
+        style="height:60%;width:100%;margin-top:15%;"
+    )
+    # Firebase Connection
+    # db = firebase.database()
+    # all_users = db.child("users").get()
+    # for user in all_users.each():
+    #     print(user.key())
+    #     print(user.val())
+    # user_ids = db.child("users").shallow().get()
+    return render_template('index.html', mymap=mymap)
 
 
 if __name__ == "__main__":
