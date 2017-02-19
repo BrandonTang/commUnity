7a001b2b85c58f5f9573474de2f82e60bbcfb79b
BrandonTang <blt264@nyu.edu>
2017-02-18 20:43:53 -0500

    Added projects page

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import operator
 import requests
 import json
 import hashlib
@@ -6,6 +7,7 @@ import pyrebase
 import sqlite3 as sql
 from flask import Flask, render_template, request, session, url_for, redirect, flash
 from flask_googlemaps import GoogleMaps, Map, icons
+from geopy.distance import vincenty
 
 # Initialize the app from Flask
 app = Flask(__name__)
@@ -30,7 +32,6 @@ def index():
     """
     Return the home page for commUnity that displays about, download, team, and maps sections.
     """
-    # Firebase Connection
     db = firebase.database()
     all_projects = db.child("Projects").get()
     markers_list = [{
@@ -40,7 +41,6 @@ def index():
         'infobox': "Current Location"
     }]
     for project in all_projects.each():
-        print(project.val())
         name = project.val()['name']
         description = project.val()['description']
         latitude = project.val()['latitude']
@@ -50,7 +50,6 @@ def index():
                              'lng': longitude,
                              'infobox': ("Name: " + name + "<br/>Description: " + description)
                              })
-    # Maps Section
     community_map = Map(
         identifier="community_map",
         zoom=16,
@@ -60,6 +59,48 @@ def index():
         style="height:60%;width:100%;color:black;"
     )
     return render_template('index.html', community_map=community_map)
+
+
+@app.route('/projects', methods=['GET', 'POST'])
+def projects():
+    """
+    Return the projects page that displays information for all projects based on distance.
+    """
+    # Google Maps
+    db = firebase.database()
+    all_projects = db.child("Projects").get()
+    markers_list = [{
+        'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+        'lat': 40.6939904,
+        'lng': -73.98656399999999,
+        'infobox': "Current Location"
+    }]
+    for project in all_projects.each():
+        name = project.val()['name']
+        description = project.val()['description']
+        latitude = project.val()['latitude']
+        longitude = project.val()['longitude']
+        markers_list.append({'icon': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+                             'lat': latitude,
+                             'lng': longitude,
+                             'infobox': ("Name: " + name + "<br/>Description: " + description)
+                             })
+    community_map = Map(
+        identifier="community_map",
+        zoom=16,
+        lat=40.6939904,
+        lng=-73.98656399999999,
+        markers=markers_list,
+        style="height:60%;width:100%;color:black;"
+    )
+    # Project List
+    distances = {}
+    for project in all_projects.each():
+        current_location = (40.6939904, -73.98656399999999)
+        project_location = (project.val()['latitude'], project.val()['longitude'])
+        distances[project] = "{0:.1f}".format(vincenty(current_location, project_location).miles)
+    projects_by_distance = sorted(distances.items(), key=operator.itemgetter(1))
+    return render_template('projects.html', community_map=community_map, projects=projects_by_distance)
 
 
 if __name__ == "__main__":
